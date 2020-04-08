221a0db2a818aad079bc3960b2443cbbbeeb9ccd
zyw <207656645@qq.com>
2020-04-08 15:15:05 +0800

    第一次提交

---
+++ src/test/java/com/zyw/online_exam/graduation_design/GraduationDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyw.online_exam.graduation_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
